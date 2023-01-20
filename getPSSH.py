21b8ddc1c04b64c4ef53bb8e1e32981931cb089e
rsgrt <98905739+rsgrt@users.noreply.github.com>
2023-01-21 05:50:35 +0800

    Add files via upload

--- getPSSH.py
+++ getPSSH.py
@@ -1,4 +1,7 @@
-import requests, xmltodict, json
+import requests
+import xmltodict
+import json
+
 
 def get_pssh(mpd_url):
     pssh = ''
@@ -9,9 +12,10 @@ def get_pssh(mpd_url):
         mpd = json.loads(json.dumps(xml))
         periods = mpd['MPD']['Period']
     except Exception as e:
-        pssh = input(f'\nUnable to find PSSH in MPD: {e}. \nEdit getPSSH.py or enter PSSH manually: ')
+        pssh = input(
+            f"\nUnable to find PSSH in MPD: {e}. \n\nEnter PSSH manually or 'skip': ")
         return pssh
-    try: 
+    try:
         if isinstance(periods, list):
             for idx, period in enumerate(periods):
                 if isinstance(period['AdaptationSet'], list):
@@ -22,26 +26,40 @@ def get_pssh(mpd_url):
                                     if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
                                         pssh = t["cenc:pssh"]
                             except Exception:
-                                pass   
+                                pass
                 else:
                     if period['AdaptationSet']['@mimeType'] == 'video/mp4':
-                            try:
-                                for t in period['AdaptationSet']['ContentProtection']:
-                                    if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                                        pssh = t["cenc:pssh"]
-                            except Exception:
-                                pass   
-        else:
-            for ad_set in periods['AdaptationSet']:
-                    if ad_set['@mimeType'] == 'video/mp4':
                         try:
-                            for t in ad_set['ContentProtection']:
+                            for t in period['AdaptationSet']['ContentProtection']:
                                 if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
                                     pssh = t["cenc:pssh"]
                         except Exception:
-                            pass   
+                            pass
+        else:
+            for ad_set in periods['AdaptationSet']:
+                if ad_set['@mimeType'] == 'video/mp4':
+                    try:
+                        for t in ad_set['ContentProtection']:
+                            if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
+                                pssh = t["cenc:pssh"]
+                    except Exception:
+                        pass
     except Exception:
-        pass                      
+        try:  # for IW
+            for ad_set in periods['AdaptationSet']:
+                if ad_set['@contentType'] == 'video':
+                    try:
+                        for t in ad_set['ContentProtection']:
+                            if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
+                                pssh = t["cenc:pssh"]
+                    except Exception:
+                        pass
+        except Exception:
+            pass
+
     if pssh == '':
-        pssh = input('Unable to find PSSH in mpd. Edit getPSSH.py or enter PSSH manually: ')
-    return pssh
+        pssh = input(
+            f"\nUnable to find PSSH in MPD. Enter PSSH manually or 'skip': ")
+        return pssh
+    else:
+        return pssh
--- l3dl.py
+++ l3dl.py
@@ -2,20 +2,19 @@
 # Created on: 11-10-2021, modified 1/17/23 for l3dl-re
 # Authors: -∞WKS∞- , r3n
 
-version = '0.1'
-import os
-import base64
-import headers
-import sqlite3
-import requests
-import argparse
-import xmltodict
-import subprocess
-
-from getPSSH import get_pssh
-from base64 import b64encode
-from pywidevine_.L3.cdm import cdm, deviceconfig
 from pywidevine_.L3.decrypt.wvdecryptcustom import WvDecrypt
+from pywidevine_.L3.cdm import cdm, deviceconfig
+from base64 import b64encode
+from getPSSH import get_pssh
+import subprocess
+import xmltodict
+import argparse
+import requests
+import sqlite3
+import headers
+import base64
+import os
+version = '0.1'
 
 
 def WV_Function(pssh, lic_url, cert_b64=None):
@@ -55,46 +54,58 @@ def fetch_key(pssh):
     cursor = conn.cursor()
     fetch = cursor.execute(
         f"SELECT KEYS FROM TBL_KEYS WHERE PSSH='{pssh}'").fetchall()
+    # edit this for cases that no pssh can be extracted from MPD
     fetched_keys = fetch[0][0]
     conn.commit()
     conn.close()
     return fetched_keys
 
 
-def download(keys):        
-    if select_ and not os.path.exists(f'{cd}\\output\\{name}.mkv'):
-        os.system(f'N_m3u8DL-RE.exe "{mpd_url}" --log-level ERROR --binary-merge --live-real-time-merge --mp4-real-time-decryption --key {keys} -M format=mkv --del-after-done --save-name "{name}" --save-dir "{cd}\\output" --use-shaka-packager -mt TRUE --thread-count 12')
-    elif not select_ and not os.path.exists(f'{cd}\\output\\{name}.mkv'):
-        print(f'Processing {name} | Download, decrypt, and muxing may take some time.')
-        subprocess.call(f'N_m3u8DL-RE.exe "{mpd_url}" --log-level ERROR --binary-merge --live-real-time-merge --mp4-real-time-decryption --key {keys} -M format=mkv --del-after-done -ss all -sa best -sv best --save-name "{name}" --save-dir "{cd}\\output" --use-shaka-packager -mt TRUE --thread-count 12')
-    else:
-        print(f'{name} already on output folder. Skipped.')
-
-
-def start_process():
-    pssh = get_pssh(mpd_url)
-    print(f'\nPSSH: {pssh}')
-    cached = find_pssh(pssh)
-    if cached:
-        keys = fetch_key(pssh)
-        print(f'Cached key found: {fetch_key(pssh)}')
-        if not keys_:
-            download(keys)
-    else:
-        if lic_url != None:
-            correct, keys = WV_Function(pssh, lic_url)
-
-            if keys:
-                for key in keys:
-                    print(f'Keys: {key}')
-                keys = key
-                insert_table(pssh, keys, lic_url, mpd_url)
-                if keys_:
-                    print(f'Done fetching keys for {name}')
-                else:
-                    download(keys)
+def download(keys):
+    if mpd_url != None:
+        if select_ and not os.path.exists(f'{cd}\\output\\{name}.mkv'):
+            os.system(f'N_m3u8DL-RE.exe "{mpd_url}" --log-level ERROR --binary-merge --live-real-time-merge --mp4-real-time-decryption --key {keys} -M format=mkv --del-after-done --save-name "{name}" --save-dir "{cd}\\output" --use-shaka-packager -mt TRUE --thread-count 12')
+        elif not select_ and not os.path.exists(f'{cd}\\output\\{name}.mkv'):
+            print(
+                f'Processing {name} | Download, decrypt, and muxing may take some time.')
+            subprocess.call(f'N_m3u8DL-RE.exe "{mpd_url}" --log-level ERROR --binary-merge --live-real-time-merge --mp4-real-time-decryption --key {keys} -M format=mkv --del-after-done -ss all -sa best -sv best --save-name "{name}" --save-dir "{cd}\\output" --use-shaka-packager -mt TRUE --thread-count 12')
         else:
-            print(f'You have no license URL for {name}. Try again.')
+            print(f'{name} already on output folder. Skipped.')
+    else:
+        print('No MPD URL supplied. Try again.')
+
+
+def start_():
+    if pssh_:
+        pssh = pssh_
+    else:
+        pssh = get_pssh(mpd_url)
+
+    if pssh != 'skip':
+        print(f'\nPSSH: {pssh}')
+        cached = find_pssh(pssh)
+        if cached:
+            keys = fetch_key(pssh)
+            print(f'Cached key found for {name}: {fetch_key(pssh)}')
+            if not keys_:
+                download(keys)
+        else:
+            if lic_url != None:
+                correct, keys = WV_Function(pssh.strip(), lic_url.strip())
+
+                if keys:
+                    for key in keys:
+                        print(f'Key: {key}')
+                        insert_table(pssh, key, lic_url, mpd_url)
+                    # consider multi-keys scenario, pssh to kid?
+                    if keys_:
+                        print(f'Done fetching keys for {name}.')
+                    else:
+                        download(keys)
+            else:
+                print(f'You have no license URL for {name}. Try again.')
+    else:
+        print(f'Skipped {name}.')
 
 
 print(f'''\n
@@ -108,13 +119,23 @@ r3n@RSG                                  {version}
                                                  
     -- L3DL REscripted Widevine Downloader --''')
 
-parser = argparse.ArgumentParser(description='L3DL-RE widevine downloader - r3n@RSG')
-parser.add_argument('-m', '--manifest', type=str, metavar='', help="your mpd/m3u8 link")
-parser.add_argument('-l', '--license', type=str, metavar='', help="license url link")
-parser.add_argument('-o', '--output', type=str.lower, metavar='', help="output file name")
-parser.add_argument('--select', action='store_true', help="manually pick what to download")
-parser.add_argument('--keys', action='store_true', help="keys only, don't download")
+parser = argparse.ArgumentParser(
+    description='L3DL-RE widevine downloader - r3n@RSG')
+parser.add_argument('-m', '--manifest', type=str,
+                    metavar='', help="your mpd/m3u8 link")
+parser.add_argument('-l', '--license', type=str,
+                    metavar='', help="license url link")
+parser.add_argument('-o', '--output', type=str.lower,
+                    metavar='', help="output file name")
+parser.add_argument('-p', '--pssh', type=str,
+                    metavar='', help="supply pssh instead of extracting from manifest")
 parser.add_argument('--batch', help="batch download mode. what file to open?")
+parser.add_argument('--select', action='store_true',
+                    help="manually pick what to download")
+parser.add_argument('--keys', action='store_true',
+                    help="keys only, don't download")
+parser.add_argument('--shut', action='store_true',
+                    help="shutdown windows after the process")
 
 
 args = parser.parse_args()
@@ -124,6 +145,8 @@ name = args.output
 select_ = args.select
 keys_ = args.keys
 batch_mode = args.batch
+shutdown = args.shut
+pssh_ =  args.pssh
 
 cd = os.getcwd()
 
@@ -139,8 +162,12 @@ if batch_mode:
         name = fields[0]
         mpd_url = fields[1]
         lic_url = fields[2]
-        start_process()
+        start_()
 else:
-    start_process()
+    start_()
 
-#TODO: support for multi-keys usecase, add pssh to kid?
+
+if shutdown:
+    os.system(f'shutdown /f /s')
+
+# TODO: support for multi-keys usecase
